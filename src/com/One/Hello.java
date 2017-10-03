b48a7192ea00c8eb967cb5ccc4d00a6595efb963
tt8871720 <tt8871720@gmail.com>
2017-10-03 16:26:47 +0800

    fist commit

---
+++ src/com/One/Hello.java
@@ -0,0 +1,12 @@
+package com.One;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello World");
+	
+Person p = new Person();
+p.sayHello();
+	}
+}
