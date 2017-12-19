713e12a12a9f74375c7c4af4f6b1b1746ded4bce
tt8871720 <tt8871720@gmail.com>
2017-12-19 15:26:28 +0800

    NumberTester

---
+++ src/com/ibm/Dice.java
@@ -0,0 +1,20 @@
+package com.ibm;
+
+import java.util.Random;
+
+public class Dice {
+//https://litotom.com/fju1219/
+	public static void main(String[] args) {
+		for (int i = 0; i <=10; i++) {
+		Random r = new Random();
+		int r1 = (r.nextInt(6)+1);
+		int r2 = (r.nextInt(6)+1);
+		int r3 = (r.nextInt(6)+1);
+		int r4 = (r.nextInt(6)+1);
+		System.out.print(r4 + " " +r1+ " "+ r2 + " " +r3 );
+if(r1 !=r2 & r2!=r3 & r3 != r4 & r1 != r4){
+	System.out.print(" * ");
+}
+
+System.out.println();}
+}}
---
+++ src/com/ibm/NineLazy.java
@@ -0,0 +1,28 @@
+package com.ibm;
+
+public class NineLazy {
+
+	public static void main(String[] args) {
+		for (int i = 1; i < 4; i++) {
+			for (int j = 2; j < 10; j++) {
+				if(i*j<10){System.out.print(j + "*" + i + "= " + (i*j) + "\t" );
+				}else
+				System.out.print(j + "*" + i + "=" + (i*j) + "\t" );
+			if(j==9){
+				System.out.println();
+			}
+				}	
+		
+			
+		}for (int n = 1; n <=5; n++) {for (int m = 1; m <=8; m++) {
+			System.out.print("..." + "\t");	
+		}System.out.println();
+				
+					}	for (int p = 2; p <=9; p++) {
+						System.out.print(p + "*" + "9" + "=" + (p*9) + "\t");
+						
+					}
+	
+
+	}}
+	
