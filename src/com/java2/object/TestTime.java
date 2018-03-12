4a80d34cc77d0c9a2d6bb41926b77b4e0280f659
tt8871720 <tt8871720@gmail.com>
2018-03-12 19:02:29 +0800

    no protected

--- src/com/java2/object/TestTime.java
+++ src/com/java2/object/TestTime.java
@@ -2,32 +2,34 @@ package com.java2.object;
 
 public class TestTime {
 	String section;
-	 int testingtime;
+	public int testingtime;
 	int startmins;
 	int starthour;
-	protected int hour = 0; 
-int endmins;
-int endhour;
-public TestTime(){
-	
-}
-public TestTime(int starthour , int testingtime  , int startmins , String section){
+	int hour = 0;
+	int endmins;
+	int endhour;
 
-	this.testingtime = testingtime;
-	
-	this.section = section;
-	if (testingtime > 60) {
-		endhour = hour + 1;
-		 endmins = testingtime - 60 +  startmins;
-		if(testingtime -60 > 0) {
-			endhour = hour + 2;
-			endmins= testingtime - 120 + startmins;
+	public TestTime() {
+
+	}
+
+	public TestTime(int starthour, int testingtime, int startmins, String section) {
+
+		this.testingtime = testingtime;
+
+		this.section = section;
+		if (testingtime > 60) {
+			endhour = hour + 1;
+			endmins = testingtime - 60 + startmins;
+			if (testingtime - 60 > 0) {
+				endhour = hour + 2;
+				endmins = testingtime - 120 + startmins;
+			}
 		}
-	}	
 	}
 
 	public void print() {
 
 		System.out.print(section + ":" + starthour + startmins + "~" + endhour + endmins);
 	}
-	}
+}
