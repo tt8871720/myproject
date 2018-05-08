c9c7ac4ee4d4ef7abe8f9e1d6ab26541ceef9533
tt8871720 <tt8871720@gmail.com>
2018-05-08 15:11:20 +0800

    List

---
+++ src/com/stepik/List.java
@@ -0,0 +1,14 @@
+package com.stepik;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class List {
+
+	public static void main(String[] args) {
+		ArrayList<Integer> list = new ArrayList<>(Arrays.asList(2,1,0,7));
+System.out.println(list);
+		
+	}
+
+}
