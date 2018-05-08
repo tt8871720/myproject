9119b6244d21d75fcd6b3d90264b47a8798a811e
tt8871720 <tt8871720@gmail.com>
2018-05-08 15:24:09 +0800

    strArray

--- src/com/stepik/List.java
+++ src/com/stepik/List.java
@@ -5,10 +5,12 @@ import java.util.Arrays;
 
 public class List {
 
-	public static void main(String[] args) {
-		ArrayList<Integer> list = new ArrayList<>(Arrays.asList(2,1,0,7));
-System.out.println(list);
-		
+//	public static void main(String[] args) {
+//		ArrayList<Integer> list = new ArrayList<>(Arrays.asList(2,1,0,7));
+//System.out.println(list);}
+	ArrayList<String> nameList = new ArrayList<>(Arrays.asList("Mr.Green", "Mr.Yellow", "Mr.Red"));		
+	for(String name : nameList){
+		System.out.println();
 	}
 
-}
+}}
