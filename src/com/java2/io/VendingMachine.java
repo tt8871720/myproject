6684d33d66ec2734ee898fcff7cb2c6b00e96417
tt8871720 <tt8871720@gmaul.com>
2018-06-12 15:49:21 +0800

    Vending Machine1

--- src/com/java2/io/VendingMachine.java
+++ src/com/java2/io/VendingMachine.java
@@ -10,8 +10,22 @@ public class VendingMachine {
 List<Drink> drinks = new ArrayList<>();
 VendingMachine vm = new VendingMachine();
 
-
-
 public void on(){
+	System.out.print("請投幣");
+for (int i = 0; i < drinks.size(); i++) {
+	Drink drink = drinks.get(i);
+	System.out.println(drink.getId() + "\t" + drink.getName() + "\t" + drink.getPrice());
+}
+int data = -1;
+Scanner scanner = new Scanner(System.in);
+while(data != 0){
+	data = scanner.nextInt();
+	switch(data){
+	case 0 :
+
+		break;
+	}
 	
+}
+
 }}
--- src/com/java2/io/VendingMain.java
+++ src/com/java2/io/VendingMain.java
@@ -1,45 +1,37 @@
 package com.java2.io;
 
 import java.io.BufferedReader;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class VendingMain extends Drink{
+public class VendingMain {
 	/*此賣機可投入5 , 10 , 50
 	 * 還錢時也是,按0時結束*/
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		List<Drink> drinks = new ArrayList<>();
-		VendingMachine vm = new VendingMachine();
-		try {
-			
-			FileReader fr = new FileReader("Vending.txt");
-			BufferedReader bf = new BufferedReader(fr);
-			try {
-				String line = bf.readLine();
-				String [] token = line.split(",");
-				for (int i = 0; i < token.length; i++) {
-			int name = Integer.parseInt(token[1]);
-			
-					int amount = Integer.parseInt(token[1]);
-				}
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			
-			
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-		
-		//on();
+	VendingMachine vm = new VendingMachine();
+FileReader fr;
+try {
+	fr = new FileReader("Vending.txt");
+	BufferedReader bf = new BufferedReader(fr);
+	String line = bf.readLine();
+	String[] tokens = line.split(",");
+	int count = Integer.parseInt(tokens[0]);
+	for (int i = 0; i < count; i++) {
+		String name = tokens[i*2+1];
+		int price  = Integer.parseInt(tokens[i*2+2]);
+vm.drinks.add(new Drink(i+1 , name , price));
 	}
-
+	vm.on();
+	
+} catch (FileNotFoundException e) {
+	// TODO Auto-generated catch block
+	e.printStackTrace();
+} catch (IOException e) {
+	// TODO Auto-generated catch block
+	e.printStackTrace();
+}
+	}
 }
