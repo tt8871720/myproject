f2005a4f86424c219ee8ba766e0bb0e8fea8af6e
tt8871720 <tt8871720@gmail.com>
2018-06-14 22:21:06 +0800

    Vending-balacne

--- src/com/java2/io/VendingMachine.java
+++ src/com/java2/io/VendingMachine.java
@@ -5,22 +5,45 @@ import java.util.List;
 import java.util.Scanner;
 
 public class VendingMachine {
-List<Drink> drinks = new ArrayList<>();
-public void on() {
-	System.out.println("請投錢");
-	for (int i = 0; i < drinks.size(); i++) {
-		Drink drink = drinks.get(i);
-		System.out.println(drink.getId() + "\t" + drink.getName() + "\t" + 
-		drink.getPrice());
-	}
-	Scanner scanner = new Scanner(System.in);
-	int data = 1;
-	while(data != 0) {
-		data = scanner.nextInt();
-		switch(data){
-		case 0:
-			break;
+	List<Drink> drinks = new ArrayList<>();
+
+	public void on() {
+		int money = 0;
+		for (int i = 0; i < drinks.size(); i++) {
+			Drink drink = drinks.get(i);
+			System.out.println(drink.getId() + "\t" + drink.getName() + "\t" + drink.getPrice());
+		}
+		Scanner scanner = new Scanner(System.in);
+		int data = 1;
+		while (data != 0) {
+			System.out.println("請投錢，現在＄: " + money);
+			data = scanner.nextInt();
+			switch (data) {
+			case 50:
+				money = 50 + money;
+				break;
+			case 10:
+				money = 10 + money;
+				break;
+			case 5:
+				money = 5 + money;
+				break;
+			case 1:
+				money = money - 25;
+				System.out.println("your Coke");
+				break;
+			case 2:
+				money = money - 30;
+				System.out.println("your Apple Juice");
+				break;
+			case 3:
+				money = money - 20;
+				System.out.println("your Sprite");
+				break;
+			}
+			if (money < 0) {
+				System.out.println("餘額不足");
+			}
 		}
 	}
-}
 }
