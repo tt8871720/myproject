c02f71e1f76b806e861fbea657c1c72d7d4c3ecf
tt8871720 <tt8871720@gmail.com>
2018-06-18 10:40:21 +0800

    bingo shuffle2

--- src/com/java2/finialpractice/Bingo.java
+++ src/com/java2/finialpractice/Bingo.java
@@ -9,11 +9,15 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.Random;
+import java.util.Scanner;
 
 public class Bingo {
+public class Play{
+	Scanner scan = new Scanner(System.in);
 	
+}
 	public static void main(String[] args) {
-List<Integer> num = new ArrayList<>();
+		List<Integer> num = new ArrayList<>();
 		try {
 			FileReader fi = new FileReader("bingo.txt");
 			BufferedReader bu = new BufferedReader(fi);
@@ -22,10 +26,20 @@ List<Integer> num = new ArrayList<>();
 			int frame = (int) Math.sqrt(data);
 			System.out.println("邊寬 :" + frame);
 			for (int i = 1; i <= data; i++) {
-				num.add(i);}
-			 Collections.shuffle(num);
-				System.out.println(num + "\t");
-			
+				// System.out.println(i);
+				num.add(i);
+				Collections.shuffle(num);}
+				// System.out.println(num + "\t");}
+				for (int j = 0; j < num.size(); j++) {				
+	if (num.get(j) < 10) {
+						System.out.print(num.get(j) + "\t");
+					} else {
+						System.out.print( num.get(j) + "\t");
+					}
+					if (j % 5 == 4) {
+						System.out.println();
+					}
+			}
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -33,8 +47,6 @@ List<Integer> num = new ArrayList<>();
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-
 
 	}
 
